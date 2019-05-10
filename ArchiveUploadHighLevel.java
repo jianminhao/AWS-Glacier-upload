59781c04f5367f74a6e73adc8e78f69d23f3e5c6
J W <jianminhao@gmail.com>
2019-05-10 13:56:28 -0700

    Add files via upload

---
+++ ArchiveUploadHighLevel.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
+import com.amazonaws.services.glacier.transfer.UploadResult;
+
+
+public class ArchiveUploadHighLevel {
+    public static String vaultName = "t1";
+    public static String archiveToUpload = "c:\\test\\t1.txt";
+    
+    public static AmazonGlacierClient client;
+    
+    public static void main(String[] args) throws IOException {
+        
+        
+    	ProfileCredentialsProvider credentials = new ProfileCredentialsProvider();
+    	
+        client = new AmazonGlacierClient(credentials);
+        client.setEndpoint("https://glacier.us-west-2.amazonaws.com/");
+
+        try {
+            ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
+            
+            UploadResult result = atm.upload(vaultName, "my archive " + (new Date()), new File(archiveToUpload));
+            System.out.println("Archive ID: " + result.getArchiveId());
+            
+        } catch (Exception e)
+        {
+            System.err.println(e);
+        }
+    }
+}
